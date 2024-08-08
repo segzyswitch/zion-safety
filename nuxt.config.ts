d1a97882a02505ce753241ff596b4fd02a2f455f
segzyswitch <segzyswitch99@gmail.com>
2024-08-08 02:41:14 +0100

    update page meta

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,7 +13,7 @@ export default defineNuxtConfig({
       meta: [
         { charset: 'utf-8' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-        { hid: 'description', name: 'description', content: '' },
+        { hid: 'description', name: 'description', content: 'THE ROLE OF ARTIFICIAL INTELLIGENCE(AI) IN WEB DEVELOPMENT FOR INCREASING EFFICIENCY, ENHANCED SAFETY, AND INTEGRITY RELIABILITY IN AN ORGANIZATION' },
         { name: 'format-detection', content: 'telephone=no' }
       ],
       link: [
