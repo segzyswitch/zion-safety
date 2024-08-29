d524e6ac6e054e8b4205dcf966575747cf33e6ce
segzyswitch <segzyswitch99@gmail.com>
2024-08-29 12:37:27 +0100

    images fixedd

--- components/page-header.vue
+++ components/page-header.vue
@@ -9,7 +9,7 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav m-auto mb-2 mb-lg-0">
             <li class="nav-item">
-              <RouterLink to="/" class="nav-link pe-4 active" aria-current="page">Home</RouterLink>
+              <RouterLink to="/" class="nav-link pe-4" aria-current="page">Home</RouterLink>
             </li>
             <li class="nav-item">
               <RouterLink to="/shop" class="nav-link pe-4">Shop</RouterLink>
